e3a3835671deea458474b5452c03e088665fd481
zhangbai-mac <zhangbai2008@gmail.com>
2016-04-08 16:22:55 +0800

    1.init ostf network

--- openstack/kilo/configfile_template/neutron-server/ostf_network_init.sh
+++ openstack/kilo/configfile_template/neutron-server/ostf_network_init.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 #configure env var
+export OS_TOKEN=<ADMIN_TOKEN>
+export OS_URL=http://<KEYSTONE_VIP>:35357/v2.0
 
-export OS_SERVICE_TOKEN=123456
-export OS_SERVICE_ENDPOINT=http://<KEYSTONE_VIP>:35357/v2.0
-
-export OS_USERNAME=admin
-export OS_PASSWORD=123456
-export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://<KEYSTONE_VIP>:35357/v2.0
+export LC_ALL=C
+export OS_NO_CACHE='true'
+export OS_TENANT_NAME='admin'
+export OS_PROJECT_NAME='admin'
+export OS_USERNAME='admin'
+export OS_PASSWORD='<KEYSTONE_ADMIN_PASSWORD>'
+export OS_AUTH_URL='http://<KEYSTONE_VIP>:5000/v2.0/'
+export OS_AUTH_STRATEGY='keystone'
+export OS_REGION_NAME='RegionOne'
+export CINDER_ENDPOINT_TYPE='internalURL'
+export GLANCE_ENDPOINT_TYPE='internalURL'
+export KEYSTONE_ENDPOINT_TYPE='internalURL'
+export NOVA_ENDPOINT_TYPE='internalURL'
+export NEUTRON_ENDPOINT_TYPE='internalURL'
+export OS_ENDPOINT_TYPE='internalURL'
+export OS_VOLUME_API_VERSION=2
 
 echo 'start to init ostf network==========='
 
--- openstack/kilo/ostf/initOSTF.py
+++ openstack/kilo/ostf/initOSTF.py
@@ -90,30 +90,39 @@ if __name__ == '__main__':
     #when execute script,exec: python <this file absolute path>
     ###############################
     #REFACTOR LATER
-#     if Role.isNeutronServerRole() :
-#         NETWORK_INSTALL_TAG_FILE = '/opt/initOSTFNetwork'
-#         if os.path.exists(NETWORK_INSTALL_TAG_FILE) :
-#             print 'ostf network initted####'
-#             print 'exit===='
-#             pass
-#         else :
-#             if NeutronServerHA.isMasterNode() :
-#                 network_init_script_path = os.path.join(OPENSTACK_CONF_FILE_TEMPLATE_DIR, 'neutron-server', 'ostf_network_init.sh')
-#                 ShellCmdExecutor.execCmd('cp -r %s /opt/' % network_init_script_path)
-#                 ############
-#                 keystone_vip = JSONUtility.getValue('keystone_vip')
-#                 
-#                 ShellCmdExecutor.execCmd('chmod 777 /opt/ostf_network_init.sh')
-#                 FileUtil.replaceFileContent('/opt/ostf_network_init.sh', '<KEYSTONE_VIP>', keystone_vip)
-#                 
-#                 ###########
-#                 ShellCmdExecutor.execCmd('bash /opt/ostf_network_init.sh')
-#                 pass
-#     
-#             #mark: OSTF network is installed
-#             os.system('touch %s' % NETWORK_INSTALL_TAG_FILE)
-#             pass
-#         pass
+    if Role.isNeutronServerRole() :
+        NETWORK_INSTALL_TAG_FILE = '/opt/openstack_conf/tag/install/initOSTFNetwork'
+        if os.path.exists(NETWORK_INSTALL_TAG_FILE) :
+            print 'ostf network initted####'
+            print 'exit===='
+            pass
+        else :
+            from openstack.common.serverSequence import ServerSequence
+            neutron_server_ips = JSONUtility.getValue('neutron_ips')
+            neutron_ip_list = neutron_server_ips.strip().split(',')
+            output, exitcode = ShellCmdExecutor.execCmd('cat /opt/localip')
+            localIP = output.strip()
+            if ServerSequence.getIndex(neutron_ip_list, localIP) == 0:
+                network_init_script_path = os.path.join(OPENSTACK_CONF_FILE_TEMPLATE_DIR, 'neutron-server', 'ostf_network_init.sh')
+                ShellCmdExecutor.execCmd('cp -r %s /opt/openstack_conf/scripts' % network_init_script_path)
+                ############
+                keystone_vip = JSONUtility.getValue('keystone_vip')
+                admin_token = JSONUtility.getValue('admin_token')
+                keystone_admin_password = JSONUtility.getValue('keystone_admin_password')
+                 
+                ShellCmdExecutor.execCmd('chmod 777 /opt/openstack_conf/scripts/ostf_network_init.sh')
+                FileUtil.replaceFileContent('/opt/openstack_conf/scripts/ostf_network_init.sh', '<KEYSTONE_VIP>', keystone_vip)
+                FileUtil.replaceFileContent('/opt/openstack_conf/scripts/ostf_network_init.sh', '<ADMIN_TOKEN>', admin_token)
+                FileUtil.replaceFileContent('/opt/openstack_conf/scripts/ostf_network_init.sh', '<KEYSTONE_ADMIN_PASSWORD>', keystone_admin_password)
+                 
+                ###########
+                ShellCmdExecutor.execCmd('bash /opt/openstack_conf/scripts/ostf_network_init.sh')
+                pass
+     
+            #mark: OSTF network is installed
+            os.system('touch %s' % NETWORK_INSTALL_TAG_FILE)
+            pass
+        pass
     
     if Role.isGlanceRole() :
         #To sync image on glance hosts
