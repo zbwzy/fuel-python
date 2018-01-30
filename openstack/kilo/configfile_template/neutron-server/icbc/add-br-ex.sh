564dee83fb54bfcabe17fdb8f9b0ac714cc07697
zhangbai-mac <zhangbai2008@gmail.com>
2018-01-30 09:50:03 +0800

    []

--- openstack/kilo/configfile_template/neutron-server/icbc/add-br-ex.sh
+++ openstack/kilo/configfile_template/neutron-server/icbc/add-br-ex.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+echo "prepare rpc.py adapted to Huawei AC====="
+mv /usr/lib/python2.7/site-packages/neutron/plugins/ml2/rpc.py /home
+mv /usr/lib/python2.7/site-packages/neutron/plugins/ml2/rpc.py.huawei /usr/lib/python2.7/site-packages/neutron/plugins/ml2/rpc.py
+echo "prepare rpc.py adapted to Huawei AC####"
+
 echo "start to add ovs-bridge br-ex===="
 ovs-vsctl add-br br-ex
 ovs-vsctl add-port br-ex bond0
